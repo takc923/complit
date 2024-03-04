59a77d770842161b587c989e4420b7cb0da35461
Takeshi Kunimi <takeshi.kunimi@umitron.com>
2024-03-05 00:17:22 +0900

    Replace var with let or const

--- service_worker.js
+++ service_worker.js
@@ -1,4 +1,4 @@
-var winHistory = [-1, -1];
+let winHistory = [-1, -1];
 
 chrome.windows.onFocusChanged.addListener(newWinId => {
     if (newWinId == chrome.windows.WINDOW_ID_NONE) return;
@@ -20,14 +20,14 @@ chrome.action.onClicked.addListener(tab => {
 function combine() {
     if (winHistory.some(id => id == -1)) return;
     chrome.windows.get(winHistory[0], {populate: true}, lastWin => {
-        var tabIds = lastWin.tabs.map(tab => tab.id);
+        const tabIds = lastWin.tabs.map(tab => tab.id);
         chrome.tabs.move(tabIds, {windowId: winHistory[1], index: -1});
     });
 }
 
 function split(tabs){
-    var tabIds = tabs.map(tab => tab.id);
-    var firstTabId = tabIds.shift();
+    const tabIds = tabs.map(tab => tab.id);
+    const firstTabId = tabIds.shift();
     chrome.windows.create({tabId: firstTabId}, win => {
         chrome.tabs.move(tabIds, {windowId: win.id, index: -1});
     });
