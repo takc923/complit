cd5d9cbb2a2111c538df2617fe8685cd0ab27920
Takeshi Kunimi <takeshi.kunimi@umitron.com>
2024-03-05 00:21:39 +0900

    Replace `==` with `===`

--- service_worker.js
+++ service_worker.js
@@ -1,24 +1,24 @@
 let winHistory = [-1, -1];
 
 chrome.windows.onFocusChanged.addListener(newWinId => {
-    if (newWinId == chrome.windows.WINDOW_ID_NONE) return;
+    if (newWinId === chrome.windows.WINDOW_ID_NONE) return;
 
     winHistory.shift();
     winHistory.push(newWinId);
 });
 
 chrome.windows.onRemoved.addListener(removedWinId => {
-    winHistory = winHistory.map(id => id == removedWinId ? -1 : id);
+    winHistory = winHistory.map(id => id === removedWinId ? -1 : id);
 });
 
 chrome.action.onClicked.addListener(tab => {
     chrome.tabs.query({highlighted: true, currentWindow: true}, tabs => {
-        tabs.length == 1 ? combine() : split(tabs);
+        tabs.length === 1 ? combine() : split(tabs);
     });
 });
 
 function combine() {
-    if (winHistory.some(id => id == -1)) return;
+    if (winHistory.some(id => id === -1)) return;
     chrome.windows.get(winHistory[0], {populate: true}, lastWin => {
         const tabIds = lastWin.tabs.map(tab => tab.id);
         chrome.tabs.move(tabIds, {windowId: winHistory[1], index: -1});
